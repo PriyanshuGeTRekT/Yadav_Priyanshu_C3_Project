27116f6c064c50215519a9d69fca7d8fda081ecd
PriyanshuGeTRekT <101016255+PriyanshuGeTRekT@users.noreply.github.com>
2023-04-12 11:52:59 -0400

    Part 3: Failing test case

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -1,46 +1,46 @@
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api;
+import org.mockito.Mockito;
 
 import java.time.LocalTime;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RestaurantTest {
-    RestaurantService service;
+    static Restaurant restaurant;
     @BeforeEach
-    public void init(){
-        service=new RestaurantService();
+    public static void setup() {
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
+        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
+        System.out.println("Setup Completed");
     }
-    Restaurant restaurant;
-    //REFACTOR ALL THE REPEATED LINES OF CODE
+    @AfterAll
+    public static void teardown() {
+        restaurant = null;
+        System.out.println("Teardown Completed");
+    }
 
     //>>>>>>>>>>>>>>>>>>>>>>>>>OPEN/CLOSED<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     //-------FOR THE 2 TESTS BELOW, YOU MAY USE THE CONCEPT OF MOCKING, IF YOU RUN INTO ANY TROUBLE
-
     @Test
     public void is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time(){
-        //WRITE UNIT TEST CASE HERE
-        LocalTime openingTime=LocalTime.of(9,0);
-        LocalTime closingTime=LocalTime.of(22,0);
-        service.addRestaurant("Chefs made","Noida",openingTime,closingTime);
-        LocalTime localTime1=LocalTime.of(17,0);
-        assertTrue(localTime1.isBefore(closingTime), String.valueOf(localTime1.isAfter(openingTime)));
-
+        Restaurant restaurant1 = Mockito.spy(restaurant);
+        Mockito.when(restaurant1.getCurrentTime()).thenReturn(LocalTime.parse("13:00:00"));
+        assertTrue(restaurant1.isRestaurantOpen());
     }
 
     @Test
     public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time(){
-        //WRITE UNIT TEST CASE HERE
-        LocalTime openingTime=LocalTime.of(9,0);
-        LocalTime closingTime=LocalTime.of(22,0);
-        service.addRestaurant("Chefs made","Noida",openingTime,closingTime);
-
-        LocalTime localTime2=LocalTime.of(23,0);
-        assertFalse(localTime2.isBefore(closingTime), String.valueOf(localTime2.isAfter(openingTime)));
-
+        Restaurant restaurant1 = Mockito.spy(restaurant);
+        Mockito.when(restaurant1.getCurrentTime()).thenReturn(LocalTime.parse("04:00:00"));
+        assertFalse(restaurant1.isRestaurantOpen());
     }
 
     //<<<<<<<<<<<<<<<<<<<<<<<<<OPEN/CLOSED>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -49,24 +49,13 @@ class RestaurantTest {
     //>>>>>>>>>>>>>>>>>>>>>>>>>>>MENU<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     @Test
     public void adding_item_to_menu_should_increase_menu_size_by_1(){
-        LocalTime openingTime = LocalTime.parse("10:30:00");
-        LocalTime closingTime = LocalTime.parse("22:00:00");
-        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
-        restaurant.addToMenu("Sweet corn soup",119);
-        restaurant.addToMenu("Vegetable lasagne", 269);
 
         int initialMenuSize = restaurant.getMenu().size();
         restaurant.addToMenu("Sizzling brownie",319);
         assertEquals(initialMenuSize+1,restaurant.getMenu().size());
     }
-
     @Test
     public void removing_item_from_menu_should_decrease_menu_size_by_1() throws itemNotFoundException {
-        LocalTime openingTime = LocalTime.parse("10:30:00");
-        LocalTime closingTime = LocalTime.parse("22:00:00");
-        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
-        restaurant.addToMenu("Sweet corn soup",119);
-        restaurant.addToMenu("Vegetable lasagne", 269);
 
         int initialMenuSize = restaurant.getMenu().size();
         restaurant.removeFromMenu("Vegetable lasagne");
@@ -74,41 +63,18 @@ class RestaurantTest {
     }
     @Test
     public void removing_item_that_does_not_exist_should_throw_exception() {
-        LocalTime openingTime = LocalTime.parse("10:30:00");
-        LocalTime closingTime = LocalTime.parse("22:00:00");
-        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
-        restaurant.addToMenu("Sweet corn soup",119);
-        restaurant.addToMenu("Vegetable lasagne", 269);
+
 
         assertThrows(itemNotFoundException.class,
                 ()->restaurant.removeFromMenu("French fries"));
     }
     //<<<<<<<<<<<<<<<<<<<<<<<MENU>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-@Test
-    public void TotalAmountOf_Selected_Menu_items(){
-    LocalTime openingTime = LocalTime.parse("10:30:00");
-    LocalTime closingTime = LocalTime.parse("22:00:00");
-    restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
-    restaurant.addToMenu("Sweet corn soup",119);
-    restaurant.addToMenu("Vegetable lasagne", 269);
-    restaurant.addToMenu("Patsa Alfredo",120);
-
-    List<String> selectedItem=new ArrayList<>();
-    selectedItem.add("Sweet corn soup");
-    selectedItem.add("Patsa Alfredo");
-    int expected=239;
-    int actual=restaurant.getOrderTotal(selectedItem);
-    assertEquals(239,actual);
-
-
+    //<<<<<<<<<<<<<<<<<<<<<<<<<<Total Price calc>>>>>>>>>>>>>>
+    @Test
+    public void check_item_total_for_selected_items_from_menu() {
+        List<String> itemsSelected = Arrays.asList("Sweet corn soup","Vegetable lasagne");
+        //119+269 = 388
+        assertEquals(388,restaurant.totalValue(itemsSelected));
     }
-
-
-
-
-
-
-
-
+    //<<<<<<<<<<<<<<<<<<<<<<<<<<Total Price calc>>>>>>>>>>>>>>
 }
